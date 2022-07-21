7cf2f2279eb7df34449c867a6761634e8e02eefe
AzharHamid02 <azharhamid679@gmail.com>
2022-07-21 15:03:29 +0700

    Responsi_PWEB

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$user = 'id19301721_root';
+$pass = 'Oktober4_2002';
+$db = 'tokoonline';
+$koneksi = mysqli_connect("localhost","root","", "db_tokoonline");
+
+if($koneksi){
+    //echo "koneksi Berhasil";
+}
+?>
